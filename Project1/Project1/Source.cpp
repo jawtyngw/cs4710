b5912b994e89e0acf97d459c39ba094e49650fbb
Jawtyng Wei <waaaaa31@gmail.com>
2016-12-04 21:22:15 -0700

    Changing to green

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -18,12 +18,13 @@ DWORD WINAPI findBallMat(void* parammeter);
 bool findBallPx(Vec3b pixel);
 void findBallVid(vector<Mat> & fs);
 bool findBallVal(uchar h, uchar v);
+bool findBallGreen(uchar h, uchar v);
 
 void cutMats(vector<Mat> & fs);
 int mostPixel(vector<Mat> & fs);
 int countPixel(Mat f);
 
-void cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds);
+bool cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds);
 void xydToXYZ(vector<int> xs, vector<int> ys, vector<double> ds, vector<double> & Xs, vector<double> & Ys, vector<double> & Zs);
 void xydToXYZ(int cenX, int cenY, double diameter, double & x, double & y, double & z);
 void curveFit(vector <double> & Xs, vector <double> & Ys, vector <double> & Zs, ofstream & f);
@@ -34,7 +35,10 @@ void findLongest(vector<int> & xs, vector<int> & ys, int & x1, int & y1, int & x
 void findShortest(int x, int y, vector<int> & xs, vector<int> & ys, int & x2, int & y2);
 void findLongest(int x, int y, vector<int> & xs, vector<int> & ys, int & x2, int & y2);
 int dis(int x1, int y1, int x2, int y2);
-bool recording = false;
+bool recording = true;
+int dbx = 0;
+int dby = 0;
+double dbd = 0.0;
 
 struct FindBallParams
 {
@@ -45,7 +49,6 @@ struct FindBallParams
 	double* ds;
 };
 
-
 Mat seeNoise(Mat frame) {
 	Mat hsv;
 	cvtColor(frame, hsv, CV_BGR2HSV);
@@ -54,7 +57,7 @@ Mat seeNoise(Mat frame) {
 	for (int y = 0; y < frame.rows; y++) {
 		for (int x = 0; x < frame.cols; x++) {
 			Vec3b pixel = hsv.at<Vec3b>(y, x);
-			if (findBallPx(pixel)) {
+			if (findBallGreen(pixel.val[0], pixel.val[1])) {
 				copy.at<Vec3b>(y, x).val[0] = 255;
 				copy.at<Vec3b>(y, x).val[1] = 255;
 				copy.at<Vec3b>(y, x).val[2] = 0;
@@ -84,7 +87,7 @@ int main()
 	int frame_width = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
 	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
-	//VideoWriter video("C:\\General use\\Homework\\CS 4710\\data\\out.avi", CV_FOURCC('m', 'p', '4', 'v'), 30, Size(frame_width, frame_height), true);
+	VideoWriter video("C:\\General use\\Homework\\CS 4710\\data\\out.avi", CV_FOURCC('m', 'p', '4', 'v'), 30, Size(frame_width, frame_height), true);
 
 	const int count = 150;
 	vector<Mat> fs;
@@ -98,11 +101,15 @@ int main()
 
 		if (recording)
 		{
-			//video << frame;
+			video << frame;
 			char c = (char)waitKey(33);
 			if (c == 27) break;
 		}
 	}
+
+	video.release();
+
+	return 0;
 
 	//if (!recording)
 	//findBall(frames);
@@ -117,18 +124,28 @@ int main()
 	duration = (clock() - start) / (double)CLOCKS_PER_SEC;
 	cout << "It takes " << duration << " seconds to do the first cut" << endl << endl;
 
-	//for (int i = 0; i < fs.size(); i++) {
-	//	Mat newframe = seeNoise(fs[i]);
-	//	string name = "C:\\General use\\Homework\\CS 4710\\data\\" + to_string(i) + ".png";
-	//	imwrite(name, newframe);
-	//	//video << newframe;
-	//}
+	// debug
+	vector<Mat> tsn;
 
-	//video.release();
+	for (int i = 0; i < fs.size(); i++) {
+		Mat newframe = seeNoise(fs[i]);
+		tsn.push_back(newframe);
+		string name = "C:\\General use\\Homework\\CS 4710\\data\\" + to_string(i) + ".png";
+		imwrite(name, newframe);
+		//video << newframe;
+	}
 
 	findBallVid(fs);
 
 	vcap.release();
+
+	
+	Mat in;
+	in = imread("C:\\General use\\Homework\\CS 4710\\data\\0.png", CV_LOAD_IMAGE_COLOR);
+	Mat out = in.clone();
+	circle(out, Point(dbx, dby), (int)dbd / 2, Scalar(0, 0, 255), 1, 8, 0);
+	imwrite("C:\\General use\\Homework\\CS 4710\\data\\out.png", out);
+	return 0;
 
 	int wait;
 	cin >> wait;
@@ -137,7 +154,6 @@ int main()
 }
 
 void findBallVid(vector<Mat> & frames) {
-
 
 	clock_t start;
 	double duration;
@@ -181,7 +197,12 @@ void findBallVid(vector<Mat> & frames) {
 		cout << "i: " << i << " x: " << xs[i] << " y: " << ys[i] << " d: " << ds[i] << endl;
 	}*/
 
-	cancelNoise(xs, ys, ds);
+	if (!cancelNoise(xs, ys, ds)) {
+		ofstream f;
+		f.open("C:\\General use\\Homework\\CS 4710\\data\\func.txt");
+		f.close();
+		return;
+	}
 
 	cout << "Noise cancel clear" << endl << endl;
 
@@ -198,7 +219,7 @@ void findBallVid(vector<Mat> & frames) {
 
 	curveFit(Xs, Ys, Zs, f);
 
-	cout << "Curvefit cancel clear" << endl << endl;
+	cout << "Curvefit clear" << endl << endl;
 
 	vector<double> speeds = {};
 	getSpeed(Xs, Ys, Zs, speeds);
@@ -401,6 +422,11 @@ bool findBallPx(Vec3b pixel) {
 
 bool findBallVal(uchar h, uchar v) {
 	return (h > 167 || h < 6) && v > 90;
+}
+
+bool findBallGreen(uchar h, uchar v)
+ {
+	return 55 < h && h < 75 && v > 90;
 }
 
 void cutMats(vector<Mat> & fs) {
@@ -623,7 +649,15 @@ void getSpeed(vector <double> & Xs, vector <double> & Ys, vector <double> & Zs, 
 	}
 }
 
-void cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds) {
+bool cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds) {
+	
+	// debug
+	dbx = xs[0];
+	dby = ys[0];
+	dbd = ds[0];
+	double x, y, z;
+	xydToXYZ(xs[0], ys[1], ds[0], x, y, z);
+	cout << "(x, y ,z) is (" << x << ", " << y << ", " << z << ")";
 
 	int n = xs.size();
 
@@ -654,15 +688,15 @@ void cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds) {
 		}
 
 
-		if (ys.at(i) > ys.at(i - 1)) {
+		if (ys.at(i) > ys.at(i - 1)){
 			theE--;
 			break;
 		}
 	}
 
-	cout << "The second cut starts from index " << theS + xs.size() - n << " to " << theE << endl << endl;
+	cout << "The second cut starts from index " << theS << " to " << theE << endl << endl;
 
-	if (theE != theS) {
+	if (theE != 0) {
 
 		xs.erase(xs.begin() + theE, xs.end());
 		ys.erase(ys.begin() + theE, ys.end());
@@ -673,9 +707,12 @@ void cancelNoise(vector<int> & xs, vector<int> & ys, vector<double> & ds) {
 		ds.erase(ds.begin(), ds.begin() + theS + 1);
 
 	}
+
+	return theE > 1 + theS;
 }
 
-void xydToXYZ(vector<int> xs, vector<int> ys, vector<double> ds, vector<double> & Xs, vector<double> & Ys, vector<double> & Zs) {
+void xydToXYZ(vector<int> xs, vector<int> ys, vector<double> ds, vector<double> & Xs, vector<double> & Ys, vector<double> & Zs)
+{
 	for (int i = 0; i < xs.size(); i++) {
 		double X = 0;
 		double Y = 0;
@@ -697,8 +734,8 @@ void xydToXYZ(int cenX, int cenY, double diameter, double & x, double & y, doubl
 	iPhone FOV: 58.040¢X
 	*/
 
-	double fov_v = 28 * 3.1415926535 / 180;
-	double fov = fov_v * 1920 / 1080;
+	double fov = 2 * 3.1415926535897932384626 / 180.0;
+	double fov_v = fov * 1080.0 / 1920.0;
 	double bbd = 74.68;
 
 	double depth = 0;
@@ -706,14 +743,14 @@ void xydToXYZ(int cenX, int cenY, double diameter, double & x, double & y, doubl
 	double angleH = 0;
 
 	if (diameter != 0) {
-		depth = bbd / 2 / sin(fov * diameter / 1920 / 2);
-		angleH = fov * (double)cenX / 1920;
-		angleV = fov_v * (double)cenY / 1080;
+		depth = bbd / 2.0 / sin(fov * diameter / 1920.0 / 2.0);
+		angleH = fov * (double)cenX / 1920.0;
+		angleV = fov_v * (double)cenY / 1080.0;
 	}
 
-	x = -depth * cos(fov_v / 2 - angleV) * sin(fov / 2 - angleH);
-	y = depth * sin(fov_v / 2 - angleV);
-	z = depth * cos(fov_v / 2 - angleV) * cos(fov / 2 - angleH);
+	x = -depth * cos(fov_v / 2.0 - angleV) * sin(fov / 2.0 - angleH);
+	y = depth * sin(fov_v / 2.0 - angleV);
+	z = depth * cos(fov_v / 2.0 - angleV) * cos(fov / 2.0 - angleH);
 }
 
 void findLongest(vector<int> & xs, vector<int> & ys, int & x1, int & y1, int & x2, int & y2) {
